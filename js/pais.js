55eee02c186980790e7c3bb4c9a5bc89961f57cb
aliciafp15 <aliciafp15@gmail.com>
2024-11-24 19:26:43 +0100

    metereologia

--- js/pais.js
+++ js/pais.js
@@ -58,4 +58,65 @@ class Pais {
         return `Coordenadas de la línea de meta del circuito: ${this.latitud}, ${this.longitud}`;
     }
 
+
+      // previsión del tiempo en la línea de meta del circuito para los próximos 5 días.
+    //devuelva la información del tiempo en formato JSON y con unidades de medida del sistema métrico
+    mirarTiempo() {
+        const apiKey = '533fd228654cbc50777660297ddc9c40'; // 
+        //const url = `http://api.openweathermap.org/data/2.5/forecast?q=${this.capital}&units=metric&appid=${apiKey}`;
+
+        const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.latitud}&lon=${this.longitud}&units=metric&appid=${apiKey}`;
+        //const url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${apiKey}&lat=${this.latitud}&lon=${this.longitud}`;
+    
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                const filteredList = data.list.filter(item => item.dt_txt.includes('12:00:00'));
+    
+                // const article = $('<section>').attr('data-name', 'meteo');// una seccion que contenga 5 articulos
+                const section = $('<section>');// una seccion que contenga 5 articulos
+
+    
+                filteredList.forEach(item => {
+                    // Crear un bloque article para cada día
+                    const dayArticle = $('<article>');
+    
+                    // Obtener los valores requeridos y asignar 0 si rain no está presente
+                    var tempMax = item.main.temp_max || 0;
+                    var tempMin = item.main.temp_min || 0;
+                    var humidity = item.main.humidity || 0;
+                    var rain = item.rain ? item.rain['3h'] : 0;
+
+                    var aux = item.dt_txt.split(" ");
+                    var fecha = aux[0];
+
+    
+                    // Obtener la URL base para los iconos y agregar el nombre del icono
+                    const iconUrlBase = 'http://openweathermap.org/img/wn/';
+                    const iconUrl = `${iconUrlBase}${item.weather[0].icon}.png`;
+    
+                    // Agregar elementos al bloque del día
+                    dayArticle.html(`
+                        <h3>${fecha}</h3>
+                        <p>Temp. Máxima: ${tempMax}°C</p>
+                        <p>Temp. Mínima: ${tempMin}°C</p>
+                        <p>Humedad: ${humidity}%</p>
+                        <p>Lluvia: ${rain} mm</p>
+                        <img src="${iconUrl}" alt="Icono del tiempo">
+                    `);
+    
+                    section.append(dayArticle);
+                });
+    
+                $('body').append(section);
+                console.log(filteredList);
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
+    }
+
 }
