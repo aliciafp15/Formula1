c2bab1988f1e87dc467a53f40b65dafe9e74ecdf
aliciafp15 <aliciafp15@gmail.com>
2024-12-11 00:59:06 +0100

    en calendario uso más jquery y quité strong que sobraba

--- js/agenda.js
+++ js/agenda.js
@@ -1,31 +1,31 @@
-class Agenda{
+class Agenda {
 
 
-    constructor(){
+    constructor() {
         this.apiURL = 'https://api.jolpi.ca/ergast/f1/2024/races'//info de las carreras de la temp en curso
 
 
     }
 
-    verCarreras(){
-        
+    verCarreras() {
 
-       
-         $.ajax({
+
+
+        $.ajax({
             dataType: "json",
             url: this.apiURL,
             method: 'GET',
-            success: function(data)
-            { 
-        
+            success: function (data) {
+
                 var carreras = data.MRData.RaceTable.Races;
 
-        
+
                 // Crear el contenedor principal de las carreras
                 const section = $('<section>');
-                section.html('<h3>Carreras</h3>');
-        
-                $.each(carreras, function(i, carrera){
+                const header = $('<h3>').text('Carreras');
+                section.append(header);
+
+                $.each(carreras, function (i, carrera) {
                     var nombreCarrera = carrera.raceName;
                     var nombreCircuito = carrera.Circuit.circuitName;
                     var coordenadas = carrera.Circuit.Location;
@@ -33,24 +33,24 @@ class Agenda{
                     var long = coordenadas.long;
                     var fecha = carrera.date;
                     var hora = carrera.time.split("Z")[0];
-                    
-                   
-        
+
+
+
                     // Crear el artículo para cada carrera
                     const article = $('<article>');
-                    
+
                     // Agregar contenido con un estilo similar al anterior
                     article.html(`
                         <h4>${nombreCarrera}</h4>
-                        <p><strong>Circuito:</strong> ${nombreCircuito}</p>
-                        <p><strong>Ubicación:</strong> (${lat}, ${long})</p>
-                        <p><strong>Fecha y Hora:</strong> ${fecha}, ${hora}</p>
+                        <p>Circuito: ${nombreCircuito}</p>
+                        <p>Ubicación: (${lat}, ${long})</p>
+                        <p>Fecha y Hora: ${fecha}, ${hora}</p>
                     `);
-        
+
                     // Añadir el artículo a la sección
                     section.append(article);
                 });
-        
+
                 // Insertar la sección en el documento
                 $('body > main').append(section);
 
