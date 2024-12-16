4472633b454811d9382e16b8f41821b7ecc50c53
aliciafp15 <aliciafp15@gmail.com>
2024-12-16 01:26:03 +0100

    parrafo explicativo de ayuda

--- js/memoria.js
+++ js/memoria.js
@@ -1,4 +1,9 @@
 class Memoria {
+
+    init_time;// momento en el que se inicia el juego
+    end_time; // momento en el que se termina el juego
+    cont;//contador para verificar si terminamos de voltear
+
     constructor() {
         this.elements = [
             { element: "RedBull_a", source: "multimedia/imagenes/logos/RedBull.svg" },
@@ -24,7 +29,10 @@ class Memoria {
         this.createElements();
         this.addEventListeners();
 
-       
+        //iniciliaza init_time
+        this.init_time = new Date();
+        this.cont=0;
+
     }
 
     //baraja con el algoritmo Durstenfeld
@@ -34,7 +42,7 @@ class Memoria {
             [this.elements[i], this.elements[j]] = [this.elements[j], this.elements[i]];
         }
     }
-    unflipCards(){
+    unflipCards() {
         this.lockBoard = true; // Bloquea el tablero
 
         setTimeout(() => {
@@ -42,8 +50,8 @@ class Memoria {
             this.firstCard.dataset.state = 'init';
             this.secondCard.dataset.state = 'init';
 
-            this.resetBoard(); 
-        }, 1000); 
+            this.resetBoard();
+        }, 1000);
     }
 
     //reinicia el tablero
@@ -54,53 +62,90 @@ class Memoria {
         this.lockBoard = false;
     }
 
-    
+
     // comprueba si las cartas volteadas son iguales
     //Si lo son--> disableCards;    si no--> unflipCards. (utilizando operador ternario)
-    checkForMatch(){
+    checkForMatch() {
         this.firstCard.dataset.element.split('_')[0];//compara si es el mismo numero de cartaX
         const isMatch = this.firstCard.dataset.element.split('_')[0] === this.secondCard.dataset.element.split('_')[0];
         isMatch ? this.disableCards() : this.unflipCards();
     }
 
     //deshabilita las interacciones sobre las tarjetas de memoria que ya han sido emparejadas
-    disableCards(){
+    disableCards() {
         this.firstCard.dataset.state = 'revealed';
         this.secondCard.dataset.state = 'revealed';
         this.resetBoard();
+
+        this.cont++;//aumento el contador
+
+
+         //comprobar final del juego
+         if (this.cont == (this.elements.length/2)) { //si llegó a 6, siendo 12 cartas, es que están todas
+            //iniciar endtime
+            this.end_time = new Date();
+
+            //conocer el tiempo que tardó
+            var tiempoFinal = this.calculate_date_difference();
+
+            //avisar al usuario
+            alert("El juego ha terminado!!. Duración: " + tiempoFinal)
+            //cambiar el alert por un <p>
+        }
     }
 
-    createElements(){
+     /** cuenta con los valores de las variables init_time y end_time para obtener el tiempo total invertido en resolver 
+      * horas:minutos:segundos.*/
+     calculate_date_difference() {
+        var tiempo = this.end_time - this.init_time;
+
+        var horas = Math.floor(tiempo / ((1000 * 60) * 60));
+        tiempo = tiempo - (horas * ((1000 * 60) * 60));
+
+        var minutos = Math.floor(tiempo / (1000 * 60));
+        tiempo = tiempo - (minutos * (1000 * 60));
+
+        var segundos = Math.floor(tiempo / 1000);
+
+        var resultado = (horas / 10 >= 1.0 ? horas : "0" + horas) + ":" +
+            (minutos / 10 >= 1.0 ? minutos : "0" + minutos) + ":" +
+            (segundos / 10 >= 1.0 ? segundos : "0" + segundos);
+
+        return resultado;
+
+    }
+
+    createElements() {
         const secciones = document.querySelectorAll('section')
         const tablero = secciones[1];
         for (const carta in this.elements) {
-                const elementData = this.elements[carta];
+            const elementData = this.elements[carta];
 
-                // Crear un nodo article por cada elemento
-                const card = document.createElement('article');
-                card.setAttribute('data-element', elementData.element);// valor igual al valor de la variable element extraída del JSON.
+            // Crear un nodo article por cada elemento
+            const card = document.createElement('article');
+            card.setAttribute('data-element', elementData.element);// valor igual al valor de la variable element extraída del JSON.
 
-                // Encabezado de orden 3
-                const h3 = document.createElement('h3');
-                h3.textContent = 'Tarjeta de memoria';
-                card.appendChild(h3);
+            // Encabezado de orden 3
+            const h3 = document.createElement('h3');
+            h3.textContent = 'Tarjeta de memoria';
+            card.appendChild(h3);
 
-                // Imagen de la tarjeta
-                const img = document.createElement('img');
-                img.src = elementData.source;
-                img.alt = elementData.element;
-                card.appendChild(img);
-                //solamente se visualizará cuando la tarjeta sea clicada por el usuario y se dé la vuelta.
-                 // Añadir la tarjeta al tablero de memoria
-                tablero.appendChild(card);
-                
+            // Imagen de la tarjeta
+            const img = document.createElement('img');
+            img.src = elementData.source;
+            img.alt = elementData.element;
+            card.appendChild(img);
+            //solamente se visualizará cuando la tarjeta sea clicada por el usuario y se dé la vuelta.
+            // Añadir la tarjeta al tablero de memoria
+            tablero.appendChild(card);
+
         }
     }
 
     /* recorra todas las tarjetas creadas y provoque una llamada al método flipCard de la clase Memoria cuando se lance dicho evento.
     se debe invocar utilizando la característica bind de JavaScript de la siguiente manera: this.flipCard.bind(card, this)
-    */ 
-    addEventListeners(){
+    */
+    addEventListeners() {
         const cards = document.querySelectorAll('article');
         cards.forEach(card => {
             card.addEventListener('click', this.flipCard.bind(card, this));
@@ -110,20 +155,20 @@ class Memoria {
     /*Da la vuelta a las cartas cuando son pulsadas por le usuario*/
     flipCard(game) {
         if (this.dataset.state == 'revealed') return;
-        if(game.lockBoard) return;
-        if(game.firstCard != null){//Si la tarjeta pulsada por el usuario coincide con la tarjeta pulsada anteriormente como primer elemento de la pareja actual 
+        if (game.lockBoard) return;
+        if (game.firstCard != null) {//Si la tarjeta pulsada por el usuario coincide con la tarjeta pulsada anteriormente como primer elemento de la pareja actual 
             // var primeraCarta = game.firstCard.dataset.element
             // var parejaPrimera = primeraCarta.split('_')[1];
             // var segundaCarta = this.dataset.element;
             // var segundaPareja = segundaCarta.split('_')[1];
-            if(game.firstCard.dataset.element === this.dataset.element ) return; //el método retorna yno hace nada más.
+            if (game.firstCard.dataset.element === this.dataset.element) return; //el método retorna yno hace nada más.
         }
-        this.dataset.state ='flip';
+        this.dataset.state = 'flip';
 
-        if(game.hasFlippedCard){//vigilar el clicar dos veces la misma carta
-            game.secondCard=this;
+        if (game.hasFlippedCard) {//vigilar el clicar dos veces la misma carta
+            game.secondCard = this;
             game.checkForMatch();
-        } else{
+        } else {
             game.hasFlippedCard = true;
             game.firstCard = this;//el valor de la tarjeta actual
         }
